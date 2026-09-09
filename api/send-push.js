d6e8bb88495675206d997450080c2f1438860603
bjendermisra-ops <bjendermisra@gmail.com>
2026-09-09 17:47:20 +0530

    Update send-push.js

--- api/send-push.js
+++ api/send-push.js
@@ -72,7 +72,6 @@ export default async function handler(req, res) {
                     ...(imageUrl ? { image: String(imageUrl) } : {})
                 }
             },
-
             // 🍏 4. Apple iOS APNs Alert
             apns: {
                 headers: {
